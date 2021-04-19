f45c8404cff0ec46c796ebe6573dba270e514cb2
Dias <diasermekov09@gmail.com>
2021-04-19 11:48:35 +0600

    tsis 9 snake

---
+++ Tsis 9/1.py
@@ -0,0 +1,174 @@
+
+import pygame 
+import time
+import random
+import pickle
+
+pygame.init()
+
+WHITE = (255, 255, 255)
+BLUE = (0, 0, 255)
+RED = (255, 0, 0)
+BLACK = (0, 0, 0)
+YELLOW = (255,255,102)
+GREEN = (0,255,0)
+
+WIDTH = 800
+HEIGHT = 600
+
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Snake game")
+
+snake_block_size = 10
+snake_speed = 20
+
+clock = pygame.time.Clock()
+
+font_style = pygame.font.SysFont("papyrusttc", 50)
+score_font = pygame.font.SysFont("ヒラキノ角コシックw0ttc", 35)
+
+FILE_NAME = 'snakes_saved.data'
+class Snake:
+    def __init__(self, block_size, color, coordinates,
+        keys = {'UP': K_UP, 'RIGHT':K_RIGHT,
+        'DOWN': K_DOWN, 'LEFT': K_LEFT}):
+        self.block_size = block_size
+        self.speed = block_size
+        self.color = color
+        self.keys = keys
+        self.elements = [coordinates]
+        self.dx = 0
+        self.dy = 0
+        self.ate_food = False
+    
+    def move(self, pressed_keys):
+        if pressed_keys[self.keys['UP']]:
+            self.dx = 0
+            self.dy = -1
+        elif pressed_keys[self.keys['DOWN']]:
+            self.dx = 0
+            self.dy = 1
+        elif pressed_keys[self.keys['RIGHT']]:
+            self.dx = 1
+            self.dy = 0
+        elif pressed_keys[self.keys['LEFT']]:
+            self.dx = -1
+            self.dy = 0
+        
+        old_head = self.elements[0]
+        head = [old_head[0] + self.dx * self.speed, old_head[1] + self.dy * self.speed]
+
+        if self.ate_food:
+            self.elements = [head] + self.elements
+        else:
+            self.elements = [head] + self.elements[:-1]
+        self.ate_food = False
+    
+    def draw(self, screen):
+        for item in self.elements:
+            pygame.draw.rect(screen, self.color, [*item, self.block_size, self.block_size])
+
+    def get_head_coordinates(self):
+        return self.elements[0]
+    
+    def get_length(self):
+        return len(self.elements)
+
+    def add_block(self):
+        self.ate_food = True
+    
+    def is_collide(self, walls):
+        pass
+    
+    def is_ate_food(self, food):
+        pass
+def message(font, msg, color, x, y):
+    mesg = font.render(msg, True, color)
+    screen.blit(mesg, (x, y))
+
+def game_loop():
+    game_over = False
+    game_close = False
+    choose = False
+
+    snake1 = Snake(snake_block_size, BLACK, [WIDTH // 2, HEIGHT // 2])
+    keys = {
+        'UP': pygame.K_UP,
+        'DOWN': pygame.K_DOWN,
+        'RIGHT': pygame.K_RIGHT,
+        'LEFT': pygame.K_LEFT
+    }
+    snake2 = Snake(snake_block_size, WHITE, [WIDTH // 2 + 50, HEIGHT // 2], keys=keys)
+
+    while not choose:
+        screen.fill(BLUE)
+        message(font_style, "Press space to load saved game, or other button to start a new one", RED, WIDTH // 2, HEIGHT // 2)
+        pygame.display.update()
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    try:
+                        with open(FILE_NAME, 'br') as f:
+                            snakes = pickle.load(f)
+                    except Exception as e:
+                        print(e)
+                        snakes = (snake1, snake2)
+                else:
+                    snakes = (snake1, snake2)
+                choose = True
+    
+    foodx = round(random.randrange(0, WIDTH - snake_block_size) / 10.0) * 10.0
+    foody = round(random.randrange(0, HEIGHT - snake_block_size) / 10.0) * 10.0
+    
+    while not game_close:
+        clock.tick(snake_speed)
+
+        while game_over:
+            screen.fill(BLUE)
+            message(font_style, "Game over!", RED, WIDTH // 2, HEIGHT // 2)
+            pygame.display.update()
+
+            for event in pygame.event.get():
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_q:
+                        game_over = False
+                        game_close = True
+                    if event.key == pygame.K_c:
+                        game_loop()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                game_close = True
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    with open(FILE_NAME, 'bw') as f:
+                        pickle.dump(snakes, f)
+                    game_close = True
+
+        for snake in snakes:
+            x1, y1 = snake.get_head_coordinates()
+            if x1 >= WIDTH or x1 < 0 or y1 >= HEIGHT or y1 < 0:
+                game_over = True
+            if x1 == foodx and y1 == foody:
+                foodx = round(random.randrange(0, WIDTH - snake_block_size) / 10.0) * 10.0
+                foody = round(random.randrange(0, HEIGHT - snake_block_size) / 10.0) * 10.0
+                snake.add_block()
+
+        pressed_keys = pygame.key.get_pressed()
+        for snake in snakes:
+            snake.move(pressed_keys)
+
+        screen.fill(BLUE)
+        pygame.draw.rect(screen, GREEN, [foodx, foody, snake_block_size, snake_block_size]) 
+        for snake in snakes:
+            snake.draw(screen)
+
+        message(score_font, "Your score: " + str(snake1.get_length() - 1), YELLOW, 0, 0)
+   
+        pygame.display.update()
+            
+    pygame.quit()
+    quit()
+
+if __name__ == '__main__':
+    game_loop()
---
+++ Tsis 9/tsis9.py
@@ -0,0 +1,284 @@
+import pygame
+import random
+import time
+import json
+pygame.init()
+lev1=False
+lev2=False
+cnt=0
+cnt2=0
+screen = pygame.display.set_mode((700, 700))
+pygame.font.init()
+font = pygame.font.SysFont('microsofttaile', 72)
+txt = font.render("Easy mode", True, (0, 0, 0), (255, 255, 255))
+screen.blit(txt, (160, 275))
+pygame.display.update()
+time.sleep(2)
+class Food:
+    def __init__(self):
+        self.x = random.randrange(30, 680, 20)
+        self.y = random.randrange(30, 680, 20)
+    def nextPos(self):
+        self.x = random.randrange(30, 680, 20)
+        self.y = random.randrange(30, 680, 20)
+    def draw(self):
+        pygame.draw.circle(screen, (0, 255, 0), (self.x, self.y), 10)    
+    
+class Snake():
+    def __init__(self, x, y):
+        self.size = 1
+        self.elements = [[x,y]]
+        self.side = (20,20)
+        self.dx = 20
+        self.dy = 0
+        self.speed = 5
+        self.is_add = False
+    def draw(self):
+        for element in self.elements:
+            pygame.draw.rect(screen, (255,0,0),(element,self.side))
+    
+    def add_snake(self):
+        self.size += 1
+        self.elements.append([0, 0])
+        self.is_add = False
+
+    def move(self):
+        if self.is_add:
+            self.add_snake()
+
+        for i in range(self.size - 1, 0, -1):
+            self.elements[i][0] = self.elements[i - 1][0]
+            self.elements[i][1] = self.elements[i - 1][1]
+        self.elements[0][0] += self.dx
+        self.elements[0][1] += self.dy   
+    
+    def eat(self, foodx, foody):
+        x = self.elements[0][0]
+        y = self.elements[0][1]
+        if foodx <= x <= foodx + 10 and foody <= y <= foody + 10:
+            return True
+        return False
+    def death(self):
+        x = self.elements[0][0]
+        y = self.elements[0][1]
+        if x < 19 or x > 679 or y < 19 or y > 679:
+            font = pygame.font.SysFont('microsofttaile', 72)
+            txt = font.render("Snake 2 win", True, (0, 0, 0), (255, 255, 255))
+            screen.fill((255,255,255))
+            screen.blit(txt, (160, 275))
+            pygame.display.update()
+            time.sleep(2)
+            exit()
+        for i in range(self.size - 1, 0, -1):
+            if self.elements[0][0] == self.elements[i][0] and self.elements[0][1] == self.elements[i][1]:
+                font = pygame.font.SysFont('microsofttaile', 72)
+                txt = font.render("Snake 2 win", True, (0, 0, 0), (255, 255, 255))
+                screen.fill((255,255,255))
+                screen.blit(txt, (160, 275))
+                pygame.display.update()
+                time.sleep(2)
+                exit()
+        if lev1 == True and ((x, y)==(141,141) or (x, y)==(141,541) or (x, y)==(541,141) or (x, y)==(541,141)):
+            font = pygame.font.SysFont('microsofttaile', 72)
+            txt = font.render("Snake 1 win", True, (0, 0, 0), (255, 255, 255))
+            screen.fill((255,255,255))
+            screen.blit(txt, (160, 275))
+            pygame.display.update()
+            time.sleep(2) 
+            exit()
+        if lev2 == True and ((x, y)==(241,241) or (x, y)==(241,441) or (x, y)==(441,241) or (x, y)==(441,241)):
+            font = pygame.font.SysFont('microsofttaile', 72)
+            txt = font.render("Snake 1 win", True, (0, 0, 0), (255, 255, 255))
+            screen.fill((255,255,255))
+            screen.blit(txt, (160, 275))
+            pygame.display.update()
+            time.sleep(2) 
+            exit()
+
+class Snake2():
+    def __init__(self, x, y):
+        self.size = 1
+        self.elements = [[x,y]]
+        self.side = (20,20)
+        self.dx = 0
+        self.dy = 20
+        self.speed = 5
+        self.is_add = False
+    def draw(self):
+        for element in self.elements:
+            pygame.draw.rect(screen, (0,0,255),(element,self.side))
+    
+    def add_snake(self):
+        self.size += 1
+        self.elements.append([0, 0])
+        self.is_add = False
+
+    def move(self):
+        if self.is_add:
+            self.add_snake()
+
+        for i in range(self.size - 1, 0, -1):
+            self.elements[i][0] = self.elements[i - 1][0]
+            self.elements[i][1] = self.elements[i - 1][1]
+        self.elements[0][0] += self.dx
+        self.elements[0][1] += self.dy   
+    
+    def eat(self, foodx, foody):
+        x = self.elements[0][0]
+        y = self.elements[0][1]
+        if foodx <= x <= foodx + 10 and foody <= y <= foody + 10:
+            return True
+        return False
+    def death(self):
+        x = self.elements[0][0]
+        y = self.elements[0][1]
+        if x < 19 or x > 679 or y < 19 or y > 679:
+            font = pygame.font.SysFont('microsofttaile', 72)
+            txt = font.render("Snake 1 win", True, (0, 0, 0), (255, 255, 255))
+            screen.fill((255,255,255))
+            screen.blit(txt, (160, 275))
+            pygame.display.update()
+            time.sleep(2) 
+            exit()
+        for i in range(self.size - 1, 0, -1):
+            if self.elements[0][0] == self.elements[i][0] and self.elements[0][1] == self.elements[i][1]:
+                font = pygame.font.SysFont('microsofttaile', 72)
+                txt = font.render("Snake 1 win", True, (0, 0, 0), (255, 255, 255))
+                screen.fill((255,255,255))
+                screen.blit(txt, (160, 275))
+                pygame.display.update()
+                time.sleep(2) 
+                exit()
+        if lev1 == True and ((x, y)==(141,141) or (x, y)==(141,541) or (x, y)==(541,141) or (x, y)==(541,141)):
+            font = pygame.font.SysFont('microsofttaile', 72)
+            txt = font.render("Snake 1 win", True, (0, 0, 0), (255, 255, 255))
+            screen.fill((255,255,255))
+            screen.blit(txt, (160, 275))
+            pygame.display.update()
+            time.sleep(2) 
+            exit()
+        if lev2 == True and ((x, y)==(241,241) or (x, y)==(241,441) or (x, y)==(441,241) or (x, y)==(441,241)):
+            font = pygame.font.SysFont('microsofttaile', 72)
+            txt = font.render("Snake 1 win", True, (0, 0, 0), (255, 255, 255))
+            screen.fill((255,255,255))
+            screen.blit(txt, (160, 275))
+            pygame.display.update()
+            time.sleep(2) 
+            exit()
+def score():
+    font = pygame.font.SysFont('microsofttaile', 15)
+    a=snake1.size-1
+    tx = font.render("score RSnake "+str(a), True, (255, 255, 255), (100, 100, 100))
+    screen.blit(tx, (0, 0))
+
+    b=snake2.size-1
+    tx = font.render("score BSnake "+str(b), True, (255, 255, 255), (100, 100, 100))
+    screen.blit(tx, (580, 0))    
+snake1 = Snake(100, 100)
+snake2 = Snake2(160, 100)
+food = Food()
+d=20
+m=10
+h=20
+def MediumWall(cnt):
+    if lev1:
+        if cnt==0:
+            font = pygame.font.SysFont('microsofttaile', 72)
+            txt = font.render("Medium mode", True, (0, 0, 0), (255, 255, 255))
+            screen.fill((0,0,0))
+            screen.blit(txt, (160, 275))
+            pygame.display.update()
+            time.sleep(2)
+            cnt+=1
+        snake1.speed+=2
+        pygame.draw.rect(screen,(100,100,100), (140,140, 20,20))
+        pygame.draw.rect(screen,(100,100,100), (540,140, 20,20))
+        pygame.draw.rect(screen,(100,100,100), (140,540, 20,20))
+        pygame.draw.rect(screen,(100,100,100), (540,540, 20,20))
+def HardWall(cnt2):
+    if lev2:
+        if cnt2==0:
+            font = pygame.font.SysFont('microsofttaile', 72)
+            txt = font.render("Medium mode", True, (0, 0, 0), (255, 255, 255))
+            screen.fill((0,0,0))
+            screen.blit(txt, (160, 275))
+            pygame.display.update()
+            time.sleep(2)
+            cnt2+=1
+        snake1.speed+=2
+        pygame.draw.rect(screen,(100,100,100), (240,240, 20,20))
+        pygame.draw.rect(screen,(100,100,100), (440,240, 20,20))
+        pygame.draw.rect(screen,(100,100,100), (240,440, 20,20))
+        pygame.draw.rect(screen,(100,100,100), (440,440, 20,20))
+        
+clock = pygame.time.Clock()
+game = True
+while game:
+    choose=False
+    clock.tick(snake1.speed)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            game = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                game = False
+            if event.key == pygame.K_RIGHT and snake1.dx != -d:
+                snake1.dx = d
+                snake1.dy = 0
+            if event.key == pygame.K_LEFT and snake1.dx != d:
+                snake1.dx = -d
+                snake1.dy = 0
+            if event.key == pygame.K_UP and snake1.dy != d:
+                snake1.dx = 0
+                snake1.dy = -d
+            if event.key == pygame.K_DOWN and snake1.dy != -d:
+                snake1.dx = 0
+                snake1.dy = d
+
+            if event.key == pygame.K_d and snake2.dx != -d:
+                snake2.dx = d
+                snake2.dy = 0
+            if event.key == pygame.K_a and snake2.dx != d:
+                snake2.dx = -d
+                snake2.dy = 0
+            if event.key == pygame.K_w and snake2.dy != d:
+                snake2.dx = 0
+                snake2.dy = -d
+            if event.key == pygame.K_s and snake2.dy != -d:
+                snake2.dx = 0
+                snake2.dy = d
+
+            # if event.key == pygame.K_1:
+            #     snake.is_add = True
+    screen.fill((0, 0, 0))
+    for i in range(0,700,20):
+        pygame.draw.rect(screen, (200, 200, 200), (i, 0, 1, 700),1)
+        pygame.draw.rect(screen, (200, 200, 200), (0, i, 700, 1),1)
+    pygame.draw.rect(screen, (100, 100, 100), (0, 0, 20, 700))
+    pygame.draw.rect(screen, (100, 100, 100), (0, 0, 700, 20))
+    pygame.draw.rect(screen, (100, 100, 100), (681, 0, 20, 700))
+    pygame.draw.rect(screen, (100, 100, 100), (0, 681, 700, 20))
+    if snake1.eat(food.x-20, food.y-20):
+        snake1.is_add = True
+        food.nextPos()
+    if snake2.eat(food.x-20, food.y-20):
+        snake2.is_add = True
+        food.nextPos()
+    if lev1==False:
+        if (snake1.size+snake2.size) % 12 == 0:
+            lev1==True
+    if lev2==False:
+        if (snake1.size+snake2.size) % 22 == 0:
+            lev2==True
+    MediumWall(cnt)
+    HardWall(cnt2)
+    snake1.move()
+    snake2.move()
+    snake1.draw()
+    snake2.draw()
+    food.draw()
+    snake1.death()
+    snake2.death()   
+    score()
+
+    pygame.display.update()   
